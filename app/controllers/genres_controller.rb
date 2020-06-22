e76e5f9e397f026e89f9b1b415da07f2f204e737
Anmolpreet <anmolpreet.kaur@yale.edu>
2020-06-22 13:39:02 -0400

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,36 @@
+class GenresController < ApplicationController
+
+  before_action :current_genre, only: [:show, :edit, :update]
+
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(genre_params)
+    redirect_to "/genres/#{@genre.id}"
+  end
+
+  def edit
+  end
+
+  def update
+    @genre.update(genre_params)
+    redirect_to @genre 
+  end
+
+  def current_genre 
+    @genre = Genre.find(params[:id])
+  end 
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end

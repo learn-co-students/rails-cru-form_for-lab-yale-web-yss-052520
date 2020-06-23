7291a7879d5ae547226ef6ddc6038deece5f526f
mlkelly <maurak87@gmail.com>
2020-06-23 15:55:49 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,40 @@
+class ArtistsController < ApplicationController
+    
+    before_action :current_artist, only: [:show, :edit, :update]
+    
+    def index
+        @artists = Artist.all
+    end
+
+    def show
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.create(artist_params)
+        @artist.save 
+        redirect_to artist_path(@artist)
+    end
+
+    def edit
+    end
+
+    def update
+        @artist.update(artist_params)
+        redirect_to artist_path(@artist)
+    end
+
+private
+
+    def artist_params
+        params.require(:artist).permit(:name, :bio)
+    end
+
+    def current_artist
+        @artist = Artist.find(params[:id])
+    end
+
+end

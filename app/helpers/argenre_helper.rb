cda3a1d56c1a2380d6d15dd27be22ead30d8faed
Joao Cardoso <joao.cardoso@yale.edu>
2020-06-23 18:54:17 -0400

    Done.

---
+++ app/helpers/argenre_helper.rb
@@ -0,0 +1,2 @@
+module ArgenreHelper
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a1 = Artist.new
+a1.name = 'Tom Misch'
+a1.bio = 'South London Represent'
+a1.save
+
+a2 = Artist.new
+a2.name = 'Michael Jackson'
+a2.bio = "That's Ignorant"
+a2.save
+
+g1 = Genre.new
+g1.name = 'Jazz'
+g1.save
+
+g2 = Genre.new
+g2.name = 'Rock'
+g2.save
+
+s1 = Song.new
+s1.name = 'Movie'
+s1.artist_id = 1
+s1.genre_id = 1
+s1.save
+
+s2 = Song.new
+s2.name = 'Thriller'
+s2.artist_id = 2
+s2.genre_id = 2
+s2.save
